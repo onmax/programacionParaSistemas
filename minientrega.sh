8c1fcd40a3c37ebf664de5ab5c7637a7d5c9110c
Max <maximogarciamtnez@gmail.com>
2017-11-13 12:45:27 +0100

    Cambiado fecha

--- minientrega.sh
+++ minientrega.sh
@@ -19,21 +19,21 @@ ERROR="minientrega.sh: Error, no se pudo realizar la entrega"
 
 #Si no existe la variable donde se guarda el directorio entonces:
 if test -z ${MINIENTREGA_CONF} ;then
-  echo $ERROR 
+  echo $ERROR >&2
   echo "minientrega.sh+ No es accesible el directorio \"${MINIENTREGA_CONF}\"" >&2
   exit 64
 fi
 
 #Si no es accesible el directorio:
 if test ! -d ${MINIENTREGA_CONF} ;then
-  echo $ERROR 
+  echo $ERROR >&2
   echo "minientrega.sh+ No es accesible el directorio \"${MINIENTREGA_CONF}\"" >&2
   exit 64
 else
-  cd ${MINIENTREGA_CONF}
+  cd ${MINIENTREGA_CONF}	
 	#Si no existe el archivo en dicho directorio:
   if test ! -e $1; then
-    echo $ERROR 
+    echo $ERROR >&2 
     echo "minientrega.sh+ No es accesible el fichero \"$1\"" >&2
     exit 66
   fi
@@ -44,18 +44,18 @@ source $1
 
 #Comprobamos fecha
 date -d $MINIENTREGA_FECHALIMITE>&/dev/null
+FECHAACTUAL=`date +"%Y%m%d"`
 
-if [ $? -ne "0" ] || [ $MINIENTREGA_FECHALIMITE -ge "21000101" ];then
-	echo $ERROR
-	echo "minientrega.sh+ fecha incorrecta \"$MINIENTREGA_FECHALIMITE\""
+if [ $? -ne "0" ] || [ $MINIENTREGA_FECHALIMITE -ge "21000101" ] || [ $MINIENTREGA_FECHALIMITE -ge $FECHAACTUAL ] ;then
+	echo $ERROR >&2
+	echo "minientrega.sh+ fecha incorrecta \"$MINIENTREGA_FECHALIMITE\"" >&2
 	exit 65
 fi
 
 #Comprobamos archivos
 for file in ${MINIENTREGA_FICHEROS[@]}; do
-	echo $file
-	if (test ! -e $file) || (test -r $file); then
-		echo $ERROR 
+	if test ! -e $file || test ! -r $file; then
+		echo $ERROR >&2
 		echo "minientrega.sh+ No es accesible el fichero \"$file\"" >&2
 		exit 66
 	fi
@@ -63,15 +63,15 @@ done
 
 #Comprobamos destino
 if test ! -d ${MINIENTREGA_DESTINO} ;then
-  echo $ERROR 
+  echo $ERROR >&2
   echo "minientrega.sh+ No es accesible el directorio \"${MINIENTREGA_DESTINO}\"" >&2
   exit 73
 fi
 cd ${MINIENTREGA_DESTINO}
 mkdir ${USER}
 if [ $? -ne "0" ];then
-	echo $ERROR
-	echo "minientrega.sh+ no se pudo crear el subdirectorio de entrega en \"$MINIENTREGA_DESTINO\""
+	echo $ERROR >&2
+	echo "minientrega.sh+ no se pudo crear el subdirectorio de entrega en \"$MINIENTREGA_DESTINO\""  >&2
 	exit 73
 fi
 exit 0
